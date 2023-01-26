23db4a42029bee4149fcd8a5de04798ab5a298b4
Oleg Sayakhov <gluhoy1324@gmail.com>
2023-01-26 09:09:47 +0500

    io.term: full vga16 unicode font

--- scripts/font2unit.py
+++ scripts/font2unit.py
@@ -9,15 +9,13 @@ parser.add_argument('font', help='Font filename')
 args = parser.parse_args()
 
 # load font
-chars = ['λ', ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
-
 with open(args.font, 'rb') as hlr:
     font = reader.read_bdf(hlr)
 
-    for ch in chars:
-        dat = font[ord(ch)].data
+    for ch in font.glyphs_by_codepoint:
+        dat = font[ch].data
         dat.reverse()
 
-        print(f"('{ch}', {dat}),")
+        print(f"('{chr(ch)}', {dat}),")
 
-    print(len(chars))
+    print(len(font.glyphs_by_codepoint))
