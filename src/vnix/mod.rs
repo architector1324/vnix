765102d9e26eb4f159c2cc90810c45eedc7dbc32
Oleg Sayakhov <gluhoy1324@gmail.com>
2023-02-14 23:20:47 +0500

    store: restructured tasks store: added minimum zen desktop

--- src/vnix/mod.rs
+++ src/vnix/mod.rs
@@ -56,7 +56,7 @@ pub fn vnix_entry(mut kern: Kern) -> Result<(), KernErr> {
     let mut ath: String = "super".into();
 
     'login: loop {
-        let path = Unit::parse("@task.gfx.desk.zen.login".chars()).map_err(|e| KernErr::ParseErr(e))?.0;
+        let path = Unit::parse("@task.zen.min.login".chars()).map_err(|e| KernErr::ParseErr(e))?.0;
 
         let u = kern.ram_store.load(path).ok_or(KernErr::DbLoadFault)?;
         let msg = kern.msg("super", u)?;
@@ -75,7 +75,7 @@ pub fn vnix_entry(mut kern: Kern) -> Result<(), KernErr> {
     }
 
     // zen
-    let path = Unit::parse("@task.gfx.desk.zen.welcome".chars()).map_err(|e| KernErr::ParseErr(e))?.0;
+    let path = Unit::parse("@task.zen.min.desk.load".chars()).map_err(|e| KernErr::ParseErr(e))?.0;
 
     let u = kern.ram_store.load(path).ok_or(KernErr::DbLoadFault)?;
     let msg = kern.msg(&ath, u)?;
@@ -84,7 +84,7 @@ pub fn vnix_entry(mut kern: Kern) -> Result<(), KernErr> {
 
     // λ
     loop {
-        let path = Unit::parse("@task.gfx.lambda.welcome".chars()).map_err(|e| KernErr::ParseErr(e))?.0;
+        let path = Unit::parse("@task.lambda.gfx.load".chars()).map_err(|e| KernErr::ParseErr(e))?.0;
 
         let u = kern.ram_store.load(path).ok_or(KernErr::DbLoadFault)?;
         let msg = kern.msg(&ath, u)?;
