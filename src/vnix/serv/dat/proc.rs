52ef7f8b4e5bcfd56bc2359cc4c85e1d46399a1e
Architector1324 <olegsajaxov@yandex.ru>
2023-03-31 23:21:32 +0500

    dat.proc: keys command

--- src/vnix/serv/dat/proc.rs
+++ src/vnix/serv/dat/proc.rs
@@ -4,6 +4,7 @@ use core::ops::{Generator, GeneratorState};
 use spin::Mutex;
 use alloc::rc::Rc;
 
+use alloc::vec::Vec;
 use alloc::boxed::Box;
 use alloc::string::String;
 
@@ -22,11 +23,12 @@ fn len(ath: Rc<String>, orig: Unit, msg: Unit, kern: &Mutex<Kern>) -> UnitTypeRe
     thread!({
         let (s, dat) = maybe_ok!(msg.as_pair());
         let (s, ath) = maybe!(as_async!(s, as_str, ath, orig, kern));
-        let (dat, ath) = maybe!(read_async!(dat, ath, orig, kern));
-
+        
         if s.as_str() != "len" {
             return Ok(None)
         }
+        
+        let (dat, ath) = maybe!(read_async!(dat, ath, orig, kern));
 
         // string length
         if let Some(s) = dat.clone().as_str() {
@@ -44,6 +46,22 @@ fn len(ath: Rc<String>, orig: Unit, msg: Unit, kern: &Mutex<Kern>) -> UnitTypeRe
     })
 }
 
+fn keys(ath: Rc<String>, orig: Unit, msg: Unit, kern: &Mutex<Kern>) -> UnitTypeReadAsync<Vec<Unit>> {
+    thread!({
+        let (s, map) = maybe_ok!(msg.as_pair());
+        let (s, ath) = maybe!(as_async!(s, as_str, ath, orig, kern));
+
+        if s.as_str() != "keys" {
+            return Ok(None)
+        }
+
+        let (map, ath) = maybe!(as_async!(map, as_map, ath, orig, kern));
+        let keys = map.iter().map(|(k, _)| k.clone()).collect::<Vec<_>>();
+
+        Ok(Some((keys, ath)))
+    })
+}
+
 pub fn proc_hlr(msg: Msg, _serv: ServInfo, kern: &Mutex<Kern>) -> ServHlrAsync {
     thread!({
         let ath = Rc::new(msg.ath.clone());
@@ -53,6 +71,14 @@ pub fn proc_hlr(msg: Msg, _serv: ServInfo, kern: &Mutex<Kern>) -> ServHlrAsync {
         if let Some((len, ath)) = thread_await!(len(ath.clone(), _msg.clone(), _msg.clone(), kern))? {
             let msg = Unit::map(&[
                 (Unit::str("msg"), Unit::uint(len as u32))
+            ]);
+            return kern.lock().msg(&ath, msg).map(|msg| Some(msg))
+        }
+
+        // keys
+        if let Some((keys, ath)) = thread_await!(keys(ath.clone(), _msg.clone(), _msg.clone(), kern))? {
+            let msg = Unit::map(&[
+                (Unit::str("msg"), Unit::list(&keys))
             ]);
             return kern.lock().msg(&ath, msg).map(|msg| Some(msg))
         }
