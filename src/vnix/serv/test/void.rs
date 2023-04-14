2ffcd682c1817de11d77c1327ab717cdb9384161
Architector1324 <olegsajaxov@yandex.ru>
2023-04-14 18:11:09 +0500

    test.void: implemented help handler

--- src/vnix/serv/test/void.rs
+++ src/vnix/serv/test/void.rs
@@ -1,11 +1,11 @@
 use spin::Mutex;
 use alloc::boxed::Box;
 
-use crate::thread;
+use crate::{thread, maybe_ok};
 
 use crate::vnix::core::msg::Msg;
 use crate::vnix::core::kern::Kern;
-use crate::vnix::core::unit::{Unit, UnitNew};
+use crate::vnix::core::unit::{Unit, UnitNew, UnitModify, UnitAs};
 use crate::vnix::core::serv::{ServHlrAsync, ServInfo};
 
 
@@ -13,6 +13,8 @@ pub const SERV_PATH: &'static str = "test.void";
 
 pub fn help_hlr(msg: Msg, _serv: ServInfo, kern: &Mutex<Kern>) -> ServHlrAsync {
     thread!({
+        let s = maybe_ok!(msg.msg.clone().as_str());
+
         let help = Unit::map(&[
             (
                 Unit::str("name"),
@@ -20,13 +22,30 @@ pub fn help_hlr(msg: Msg, _serv: ServInfo, kern: &Mutex<Kern>) -> ServHlrAsync {
             ),
             (
                 Unit::str("info"),
-                Unit::str("`Black hole` service\nExample: a@test.void")
-            )
+                Unit::str("'Black hole' service")
+            ),
+            (
+                Unit::str("tut"),
+                Unit::map(&[
+                    (Unit::str("info"), Unit::str("Destroy message")),
+                    (Unit::str("com"), Unit::stream_loc(Unit::str("a"), "test.void"))
+                ])
+            ),
+            (Unit::str("man"), Unit::none())
         ]);
         yield;
 
+        let res = match s.as_str() {
+            "help" => help,
+            "help.name" => maybe_ok!(help.find(["name"].into_iter())),
+            "help.info" => maybe_ok!(help.find(["info"].into_iter())),
+            "help.tut" => maybe_ok!(help.find(["tut"].into_iter())),
+            "help.man" => maybe_ok!(help.find(["man"].into_iter())),
+            _ => return Ok(None)
+        };
+
         let _msg = Unit::map(&[
-            (Unit::str("msg"), help)
+            (Unit::str("msg"), res)
         ]);
         kern.lock().msg(&msg.ath, _msg).map(|msg| Some(msg))
     })
